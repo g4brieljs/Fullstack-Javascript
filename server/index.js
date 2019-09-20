552656d94f2d1d0e7dcdc69ce9b7afba69d0bf66
Gabriel Jimenez <gabrieljimenez690@gmail.com>
2019-09-20 06:38:24 -0400

    Readme

--- server/index.js
+++ server/index.js
@@ -1,12 +1,4 @@
 // import express
 
-# Fullstack Javascript - Nodejs and Javascript ES6
-
-**First** install nodejs, and npm.
-
-**Second** to start a package.json with command `npm init`.
-**Third** install devpendecies, **body-parser** nodejs lo requiere para presentar ciertas informaciones, **Nodemon** verifica los cambios del servidor, y **Express** es el servidor que utilizamos localmente, si tienes experiencia con php, es como Xamp.
-
-### Configurando el servidor de express
 
 // configurar express
